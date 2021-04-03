1063866bf4ebff452ab5d63b9f5e4ebdca941486
Alessio Degani <alessio.degani@gmail.com>
2021-04-03 17:45:53 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # drasil
 Static HTML website generator based on directory tree
+
+This is a personal experiment, use it at your own risk :)
+
+# Install
+
+## Install from source
+
+> cd drasil
+> python setup.py install
+
+## Create a tar ball
+
+> cd drasil
+> python setup.py sdist
+> cd dist
+> tar xzf drasil-*VERSION*.tar.gz
+> cd drasil-*VERSION*
+> python setup.py install 
---
+++ drasil/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('../README.md') as f:
+    readme = f.read()
+
+with open('../LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='drasil',
+    version='0.1.0',
+    description='Static HTML website generator based on directory tree',
+    long_description=readme,
+    author='Alessio Degani',
+    author_email='alessio.degani@gmail.com',
+    url='https://github.com/adegani/drasil',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    scripts = ["drasil"],
+)
