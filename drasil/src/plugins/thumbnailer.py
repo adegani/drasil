d36b284ee85dab6e425c4107f3c22c81c762bec2
Alessio Degani <alessio.degani@gmail.com>
2021-11-20 19:06:27 +0100

    Fix: thumbnailer bug: generate wrong thumb img src

--- drasil/src/plugins/thumbnailer.py
+++ drasil/src/plugins/thumbnailer.py
@@ -6,14 +6,14 @@ from ..drasil_context import DrasilContext
 
 class DrasilPlug():
     hooks = ['thumbnailer']
-    name = 'Thumbnailer' 
+    name = 'Thumbnailer'
     description = 'Create an img tag and the thumbnail of an image'
     help_str = 'You can insert an image using '
     help_str = '[$thumbnailer:img_path:thumb_width_px:img_caption$].'
     help_str += 'The thumbnail of the image will be created (width thumb_width_px) '
     help_str += 'and the image will have the caption img_caption. '
     help_str += 'The embedded thumb will ink to the original image. '
-    
+
     def pre(self, *argv):
         pass
 
@@ -21,7 +21,7 @@ class DrasilPlug():
         caller = path.split(argv[1].current_node)[-1]
         out_dir = argv[1].output_dir
         in_dir = argv[1].src_root
-        
+
         img_link = argv[0][0]
         img_path = path.split(img_link)[0:-1][0]
         img_fixed_width = int(argv[0][1])
@@ -29,16 +29,17 @@ class DrasilPlug():
         img_name = path.split(img_link)[-1]
         thumb_name = 'thumb_' + img_name
         thumb_path = path.join(out_dir, img_path, thumb_name)
+        thumb_link = path.join(img_path, thumb_name)
         thumb_folder = path.split(thumb_path)[0]
         if not path.isdir(thumb_folder):
             os.makedirs(thumb_folder)
         if not path.exists(thumb_path):
-            image = Image.open(in_dir+img_link)
+            image = Image.open(path.join(in_dir,img_link))
             width_percent = (img_fixed_width / float(image.size[0]))
             height_size = int((float(image.size[1]) * float(width_percent)))
             image = image.resize((img_fixed_width, height_size), PIL.Image.BICUBIC)
             image.save(thumb_path)
-        img_tuple = (img_link, img_name, thumb_path, img_caption)
+        img_tuple = (img_link, img_name, thumb_link, img_caption)
         out_str = '<div class="picture">'
         out_str += '    <a href="%s" alt="%s"><img src="%s">%s</a>' % img_tuple
         out_str += '</div>'
